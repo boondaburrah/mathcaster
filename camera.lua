8478f5a07eafb3c515f4e96efdf26b38eab3f56c
Luke Berry <luke@braid.com>
2014-12-01 10:11:08 +0900

    basic drawing, no math

--- camera.lua
+++ camera.lua
@@ -9,6 +9,20 @@ local Camera = Class{
     self.fov = 90
     self.wallSegments = {}
     self.stepsize = self.fov / self.width
+  end,
+
+  segmentToHOffset = function(self, segment)
+    return math.abs((self.height / 2) / segment)
+  end,
+
+  segmentToLine = function(self, segment, x)
+    return x, (self.height / 2) - self:segmentToHOffset(segment), x, (self.height / 2) + self:segmentToHOffset(segment)
+  end,
+
+  draw = function(self)
+    for i = 1, self.width do
+      love.graphics.line(self:segmentToLine(self.wallSegments[i] or 2, i))
+    end
   end
 }
 
--- main.lua
+++ main.lua
@@ -1,8 +1,18 @@
 local Map = require "map"
+local Line = require "line"
+local Camera = require "camera"
+local Vector = require "hump.vector"
 
 function love.load()
   love.window.setMode(960, 544)
   g_map = Map()
+  g_camera = Camera(Vector.new(0, 0), 90)
+
+  -- test code/futzing around
+  g_map:addBrush(Line(-1, -1, -1, 1))
+  g_map:addBrush(Line(-1, -1, 1, -1))
+  g_map:addBrush(Line(1, -1, 1, 1))
+  g_map:addBrush(Line(1, 1, -1, 1))
 end
 
 function love.update(dt)
@@ -10,5 +20,5 @@ function love.update(dt)
 end
 
 function love.draw()
-  -- pass
+  g_camera:draw()
 end
